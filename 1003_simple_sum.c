d61a497675b84bd30916dfb62069b4372f5cb2bd
Washiul Alam Sohan <washiulalamsohan.professional@gmail.com>
2023-02-13 16:08:02 -0500

    1003 solved

---
+++ 1003_simple_sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int num_one, num_two, result;
+
+    scanf("%d %d",&num_one, &num_two);
+
+    result = num_one + num_two;
+
+    printf("SOMA = %d\n",result);
+
+    return 0;
+}
