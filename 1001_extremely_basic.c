fc9c86d156f20e4aaf42e7c3fef822ee71e2831a
Washiul Alam Sohan <washiulalamsohan.professional@gmail.com>
2023-02-13 15:58:03 -0500

    1001 solved

---
+++ 1001_extremely_basic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int num_one, num_two, result;
+
+    scanf("%d %d",&num_one, &num_two);
+
+    result = num_one + num_two;
+
+    printf("X = %d\n",result);
+
+    return 0;
+}
