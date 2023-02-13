7852d72429a7e6df88d734dd8d04656c1d78ad58
Washiul Alam Sohan <washiulalamsohan.professional@gmail.com>
2023-02-13 16:19:54 -0500

    1005 solved

---
+++ 1005_average_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    float num_one, num_two, average;
+
+    scanf("%f %f",&num_one, &num_two);
+
+    average = ((num_one*3.5)+(num_two*7.5)) / (3.5+7.5);
+
+    printf("MEDIA = %.5lf\n",average);
+
+    return 0;
+}
