5f960e94bd9b83de7f5bdcceea77521b27f8c699
Washiul Alam Sohan <washiulalamsohan.professional@gmail.com>
2023-02-13 16:05:42 -0500

    1003 solved

---
+++ 1002_area_of_circle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+#define PI (3.14159);
+
+int main()
+{
+
+    double r, rslt;
+
+    scanf("%lf",&r);
+
+    rslt = (r*r)* PI;
+
+    printf("A=%.4lf\n",rslt);
+
+    return 0;
+}
